54fb7d0bc697c74cbe37222f088f54ad81c2e8c6
Sebastian Degerman <sebbed89@hotmail.com>
2019-02-01 10:35:04 +0100

    fixes to scroll and css

--- assets/js/main.js
+++ assets/js/main.js
@@ -131,8 +131,12 @@
 	const scrollToTop = () => {
 	  const c = document.documentElement.scrollTop || document.body.scrollTop;
 	  if (c > 0) {
+		$('.to-top-btn').show();
 		window.requestAnimationFrame(scrollToTop);
 		window.scrollTo(0, c - c / 20);
+	  }
+	  else {
+		$('.to-top-btn').hide();
 	  }
 	};
 	scrollToTop();
